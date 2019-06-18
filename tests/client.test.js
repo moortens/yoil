84d6eba7220153d8b1c5bcf6494c042b9a86d5fa
moortens <mortens@gmail.com>
2019-06-18 20:18:35 +0200

    fixed test error due to api change

--- tests/client.test.js
+++ tests/client.test.js
@@ -1,24 +1,9 @@
-const IRCClient = require('../src/client');
+const irc = require('../');
 
 describe('client', () => {
-  it('defaults to port 6697 if tls is enabled', () => {
-    const client = new IRCClient({ port: 0, tls: true });
-    expect(client.config.get('port')).toBe(6697);
-  });
-
-  it('defaults to port 6667 if tls is disabled', () => {
-    const client = new IRCClient({ port: 0, tls: false });
-    expect(client.config.get('port')).toBe(6667);
-  });
-
-  it('sets port to 6697', () => {
-    const client = new IRCClient();
-    expect(client.config.get('port')).toBe(6697);
-  });
-
-  it('defaults to null if no host is given', () => {
-    const client = new IRCClient();
-
-    expect(client.config.get('host')).toBe(null);
+  it('expect client to require a config object', () => {
+    expect(() => {
+      const client = new irc.Client({});
+    }).toThrow();
   });
 });
