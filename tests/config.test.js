7cf3ca6210884aff025b9b337b85100b01707351
moortens <mortens@gmail.com>
2019-06-20 14:34:58 +0200

    tests if config is defined or not

--- tests/config.test.js
+++ tests/config.test.js
@@ -1,7 +1,19 @@
 const Config = require('../src/config');
 
 describe('config', () => {
-  it('expect config be defined', () => {
-    expect(new Config()).toBeInstanceOf(Config);
+  it('should throw when not defined', () => {
+    expect(() => {
+      new Config()
+    }).toThrow();
   });
+
+  it('should be an instance of Config if given correct parameters', () => {
+    expect(new Config({ 
+      host: '127.0.0.1',
+      port: 6667,
+      nickname: 'nickname',
+      username: 'hostname',
+      realname: 'realname',
+    })).toBeInstanceOf(Config);
+  })
 });
